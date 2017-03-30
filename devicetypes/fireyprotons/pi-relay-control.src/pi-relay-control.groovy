69b275f00cb5360d6651ec2a7e1234ebb16db2ae
FireyProtons <evonasten@gmail.com>
2017-03-29 21:06:57 -0500

    Update pi-relay-control.groovy

--- devicetypes/fireyprotons/pi-relay-control.src/pi-relay-control.groovy
+++ devicetypes/fireyprotons/pi-relay-control.src/pi-relay-control.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Some of the code taken from iBeech 
+ *  Some of the code taken from iBeech/SmartThings: https://github.com/iBeech/SmartThings
  */
 
 preferences {
