7400051ef8c301bbb585eb1663d2c6b0e288a706
Erik Von Asten <evonasten@gmail.com>
2018-02-26 09:18:54 -0600

    updated to custom RGBW bulb for Milight integration

--- devicetypes/fireyprotons/milight-rgbw.src/milight-rgbw.groovy
+++ devicetypes/fireyprotons/milight-rgbw.src/milight-rgbw.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2015 SmartThings
+ *  Copyright 2018 Erik von Asten (fireyProtons)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -10,14 +10,14 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Z-Wave RGBW Light
+ *  Milight RGBW Light
  *
- *  Author: SmartThings
+ *  Author: Erik von Asten
  *  Date: 2015-7-12
  */
 
 metadata {
-	definition (name: "Milight RGBW", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.light") {
+	definition (name: "Milight RGBW", namespace: "FireyProtons", author: "Erik von Asten") {
 		capability "Switch Level"
 		capability "Color Control"
 		capability "Color Temperature"
@@ -27,10 +27,7 @@ metadata {
 		capability "Sensor"
 
 		command "reset"
-
-		fingerprint inClusters: "0x26,0x33"
-		fingerprint deviceId: "0x1102", inClusters: "0x26,0x33"
-		fingerprint inClusters: "0x33"
+        
 	}
 
 	simulator {
